cf5d72f6aa5dce0c8ea4498876f83587a27713e2
clausbissinger <claus.bissinger@meko.de>
2022-07-28 11:27:14 +0200

    feat: add (optional) cli flags

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bytes"
 	_ "embed"
 	"encoding/xml"
+	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 	"strconv"
 	"strings"
@@ -26,42 +30,75 @@ func printTest(s formatter.JUnitTestSuite, c formatter.JUnitTestCase) {
 	if k != nil {
 		class, text = "skipped", "Skip"
 	}
-	fmt.Printf("<div class='%s' id='%s'>\n", class, id)
-	fmt.Printf("<a href='#%s'>%s <span class='badge'>%s</span></a>\n", id, c.Name, text)
-	fmt.Printf("<div class='expando'>\n")
+	fmt.Fprintf(testReport, "<div class='%s' id='%s'>\n", class, id)
+	fmt.Fprintf(testReport, "<a href='#%s'>%s <span class='badge'>%s</span></a>\n", id, c.Name, text)
+	fmt.Fprintf(testReport, "<div class='expando'>\n")
 	if f != nil {
-		fmt.Printf("<div class='content'>%s</div>\n", f.Contents)
+		fmt.Fprintf(testReport, "<div class='content'>%s</div>\n", f.Contents)
 	} else if k != nil {
-		fmt.Printf("<div class='content'>%s</div>\n", k.Message)
+		fmt.Fprintf(testReport, "<div class='content'>%s</div>\n", k.Message)
 	}
 	d, _ := time.ParseDuration(c.Time)
-	fmt.Printf("<p class='duration' title='Test duration'>%v</p>\n", d)
-	fmt.Printf("</div>\n")
-	fmt.Printf("</div>\n")
+	fmt.Fprintf(testReport, "<p class='duration' title='Test duration'>%v</p>\n", d)
+	fmt.Fprintf(testReport, "</div>\n")
+	fmt.Fprintf(testReport, "</div>\n")
 }
 
+var (
+	flagResult = flag.String("xml-result", "", "xml formatted junit test result")
+	flagReport = flag.String("html-report", "", "html report")
+)
+
+var (
+	testReport io.Writer
+)
+
 func main() {
+	// Setup
+	flag.Parse()
+	var testResult io.Reader
+	// Read Input (test result)
+	//
+	testResult = os.Stdin
+	if *flagResult != "" {
+		res, err := ioutil.ReadFile(*flagResult)
+		if err != nil {
+			panic(err)
+		}
+		testResult = bytes.NewReader(res)
+	}
+	// Output
+	//
+	testReport = os.Stdout
+	if *flagReport != "" {
+		f, err := os.OpenFile(*flagReport, os.O_RDWR|os.O_CREATE, 0755)
+		if err != nil {
+			panic(err)
+		}
+		testReport = f
+	}
+
 	suites := &formatter.JUnitTestSuites{}
 
-	err := xml.NewDecoder(os.Stdin).Decode(suites)
+	err := xml.NewDecoder(testResult).Decode(suites)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("<html>")
-	fmt.Println("<head>")
-	fmt.Println("<meta charset=\"UTF-8\">")
-	fmt.Println("<style>")
-	fmt.Println(styles)
-	fmt.Println("</style>")
-	fmt.Println("</head>")
-	fmt.Println("<body>")
+	fmt.Fprintln(testReport, "<html>")
+	fmt.Fprintln(testReport, "<head>")
+	fmt.Fprintln(testReport, "<meta charset=\"UTF-8\">")
+	fmt.Fprintln(testReport, "<style>")
+	fmt.Fprintln(testReport, styles)
+	fmt.Fprintln(testReport, "</style>")
+	fmt.Fprintln(testReport, "</head>")
+	fmt.Fprintln(testReport, "<body>")
 	failures, total := 0, 0
 	for _, s := range suites.Suites {
 		failures += s.Failures
 		total += len(s.TestCases)
 	}
-	fmt.Printf("<p>%d of %d tests failed</p>\n", failures, total)
+	fmt.Fprintf(testReport, "<p>%d of %d tests failed</p>\n", failures, total)
 	for _, s := range suites.Suites {
 		if s.Failures > 0 {
 			printSuiteHeader(s)
@@ -80,18 +117,18 @@ func main() {
 			}
 		}
 	}
-	fmt.Println("</body>")
-	fmt.Println("</html>")
+	fmt.Fprintln(testReport, "</body>")
+	fmt.Fprintln(testReport, "</html>")
 }
 
 func printSuiteHeader(s formatter.JUnitTestSuite) {
-	fmt.Println("<h4>")
-	fmt.Println(s.Name)
+	fmt.Fprintln(testReport, "<h4>")
+	fmt.Fprintln(testReport, s.Name)
 	for _, p := range s.Properties {
 		if strings.HasPrefix(p.Name, "coverage.") {
 			v, _ := strconv.ParseFloat(p.Value, 10)
-			fmt.Printf("<span class='coverage' title='%s'>%.0f%%</span>\n", p.Name, v)
+			fmt.Fprintf(testReport, "<span class='coverage' title='%s'>%.0f%%</span>\n", p.Name, v)
 		}
 	}
-	fmt.Println("</h4>")
+	fmt.Fprintln(testReport, "</h4>")
 }
